1a61da3ff07140d5f7d07f96926ebeeba5de43d2
Narendra Nath Paul <74705539+Narendra404@users.noreply.github.com>
2023-08-16 03:20:26 +0000

    Add MyTemplates

---
+++ Java_Template.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class <ClassName> {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        System.out.print();
+    }
+}
